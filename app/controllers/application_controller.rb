d8e36e1d6b377f38ef46bb56dbd9d47002a3b0c1
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-02-20 21:10:10 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,43 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get "/articles" do
+    @articles = Article.all
+    erb :index
   end
+
+  get "/articles/new" do
+    erb :new
+  end
+
+  get "/articles/:id" do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  post "/articles" do
+    title = params[:title]
+    content = params[:content]
+    article = Article.create(title: title, content: content)
+    redirect "/articles/#{article.id}"
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    article = Article.find(params[:id])
+    article.update(title: params[:title], content: params[:content])
+
+    redirect "/articles/#{article.id}"
+  end
+
+  delete '/articles/:id/delete' do
+    @article = Article.find_by_id(params[:id])
+    @article.delete
+    redirect "/articles"
+  end
+
 end
